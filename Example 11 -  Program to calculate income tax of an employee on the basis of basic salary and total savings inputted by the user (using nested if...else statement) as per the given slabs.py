1e1b668bd411c1f4cb458cee620b1536ef12a32b
inzamamulhaque96 <inzamam.au.cse@gmail.com>
2023-03-05 20:18:58 +0530

    Question Added

---
+++ Example 11 -  Program to calculate income tax of an employee on the basis of basic salary and total savings inputted by the user (using nested if...else statement) as per the given slabs.py
@@ -0,0 +1,11 @@
+'''
+Example 11: Program to calculate income tax of an employee on the basis of basic salary and total savings inputted by the user (using nested if...else statement) as per the given slabs:
+
+• No tax for individuals with income less than 2.5 lakh
+
+0%-5% tax with income 2.5 lakh to 5 lakh for different age groups
+
+• 20% tax with income 5 lakh to 10 lakh
+
+Investments up to 1.5 lakh under Sec 80C can save up to 45,000 in taxes.
+'''
