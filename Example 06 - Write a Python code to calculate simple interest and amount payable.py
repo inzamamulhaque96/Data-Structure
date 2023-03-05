3bfc059c7ddbee5f5446e483431ab58ad61a8450
inzamamulhaque96 <inzamam.au.cse@gmail.com>
2023-03-05 20:07:23 +0530

    Question Added

---
+++ Example 06 - Write a Python code to calculate simple interest and amount payable.py
@@ -0,0 +1,6 @@
+'''
+* Example 6: Write a Python code to calculate simple interest and amount payable by
+ inputting the value of principal amount and rate from the user for a time period of 5 years.
+
+* (Formula used: Simple Interest = Principal * Rate* Time/100)
+'''
