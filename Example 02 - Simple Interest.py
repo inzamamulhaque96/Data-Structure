44bc095a43d0984127b2ed24cb37e1734bdc453f
inzamamulhaque96 <inzamam.au.cse@gmail.com>
2023-03-05 19:55:17 +0530

    Question Added

---
+++ Example 02 - Simple Interest.py
@@ -0,0 +1,6 @@
+# Example 2: Lokesh has taken a loan of 40000 from Vinod at the rate of 8% per annum.
+# After 6 years, he wants to repay the loan in full including interest.
+# Write the Python code (in script mode) to calculate and display the interest and
+# total amount to be paid by Lokesh to settle his accounts.
+
+
